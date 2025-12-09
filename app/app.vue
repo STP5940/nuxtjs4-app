65c767f93c656ab34694a643a9c702a1528b8bd6
STP5940 <21144303+STP5940@users.noreply.github.com>
2025-12-09 13:32:28 +0700

    🐛 fix: Add link manifest webmanifest

--- app/app.vue
+++ app/app.vue
@@ -14,7 +14,10 @@ useHead({
     { name: "viewport", content: "width=device-width, initial-scale=1" },
     { key: "theme-color", name: "theme-color", content: color },
   ],
-  link: [{ rel: "icon", href: "/favicon.ico" }],
+  link: [
+    { rel: "icon", href: "/favicon.ico" },
+    { rel: "manifest", href: "/manifest.webmanifest" },
+  ],
   htmlAttrs: {
     lang: "en",
   },
