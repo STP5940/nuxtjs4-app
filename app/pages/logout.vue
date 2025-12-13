650232a0bfc26fc4c51a5097b380ce059b130b20
STP5940 <21144303+STP5940@users.noreply.github.com>
2025-12-13 21:29:00 +0700

    🎨 style: Update ucard to max-w-md

--- app/pages/logout.vue
+++ app/pages/logout.vue
@@ -32,7 +32,7 @@ onMounted(() => {
 
 <template>
   <div class="flex items-center justify-center min-h-screen p-4 pb-20">
-    <UCard class="w-full max-w-sm text-center">
+    <UCard class="w-full max-w-md text-center">
       <div class="py-8">
         <img src="/loading.gif" alt="Logging out..." class="h-30 w-30 mx-auto block" />
 
